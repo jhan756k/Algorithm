29a68ce31f33a002b01f997cc5937633bd809e31
Jooney <42194130+jhan756k@users.noreply.github.com>
2022-08-09 02:39:45 +0900

    some dp

---
+++ BOJ/12865.py
@@ -0,0 +1,12 @@
+n, k = map(int, input().split())
+dp = [0]*(k+1)
+
+for i in range(n):
+    w, v = map(int, input().split())
+
+    for x in range(k, w-1, -1):
+        tmp = dp[x-w] + v
+        if tmp > dp[x]:
+            dp[x] = tmp
+            
+print(dp[k])
