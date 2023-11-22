fbdb5189f8215834a9bfd0b7691997e515bc50d7
Jooney <jhan756k@gmail.com>
2023-11-22 10:49:00 +0900

    t

--- BOJ/12015.py
+++ BOJ/12015.py
@@ -1,4 +1,14 @@
 import sys
 n = int(sys.stdin.readline())
 a = list(map(int, sys.stdin.readline().split()))
+start = 0
+end = n
+while start <= end:
+    mid = (start+end)//2
 
+    
+
+    if a[mid] > a[mid+1]:
+        end = mid-1
+    else:
+        start = mid+1
---
+++ BOJ/1245.py
@@ -0,0 +1,4 @@
+import sys
+n, m = map(int, sys.stdin.readline().split())
+board = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+cnt+=1
