bdc9bdb2f91e9cf6e609b9c42a58edb17c8eb598
Jooney <jhan756k@gmail.com>
2024-11-08 11:58:24 +0900

    easy

---
+++ BOJ/18405.py
@@ -0,0 +1,18 @@
+import sys
+n, k = map(int, sys.stdin.readline().split())
+board = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+s, x, y = map(int, sys.stdin.readline().split())
+alist = []
+
+for i in range(n):
+  for j in range(n):
+    if board[i][j] != 0:
+      tmp = abs(x-1-i) + abs(y-1-j)
+      if tmp <= s:
+        alist.append((tmp, board[i][j]))
+
+if not alist:
+  print(0)
+else:
+  alist.sort(key=lambda x:(x[0], x[1]))
+  print(alist[0][1])
